48f3c204183c00f89e69767d81c6f850d7530ffb
Sachin Tripathi <sachintripathis84@gmail.com>
2019-07-10 21:51:27 +0530

    Update infinteScroll.py

--- infinteScroll.py
+++ infinteScroll.py
@@ -2,14 +2,11 @@ from selenium import webdriver
 
 def infinteScroll(driver):
     height = 0
-    i = 0
     while height < driver.execute_script("return document.body.scrollHeight"):
-        print(i)
         height = driver.execute_script("return document.body.scrollHeight")
         driver.execute_script(
             "window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(2)
-        i = i+1
     html_source = driver.page_source.encode('utf-8')
     return html_source
 
